97dbe3e981c851610382e12db17cd3aa3dde0f90
Deepak <chintadadeepak31@gmail.com>
2023-09-02 19:40:23 +0530

    understood anonymous object, inheritance and its types, single and mutli-level, multiple inheritance ambiguity

---
+++ VeryAdvCalc.java
@@ -0,0 +1,8 @@
+// the inheritance betn AdvCalc and VeryAdvCalc is single level.
+// the inheritance betn Calc and VeryAdvCalc is multi level.
+
+public class VeryAdvCalc extends AdvCalc{
+    public double power(int n1, int n2) {
+        return Math.pow(n1, n2);
+    }
+}
