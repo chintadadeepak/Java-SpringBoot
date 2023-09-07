8f98dcf59ab08e6a307f758a624846df92d2d9de
Deepak <chintadadeepak31@gmail.com>
2023-09-07 22:25:29 +0530

    understood polymorphism and its types, DMD and final keyword

--- Hello.java
+++ Hello.java
@@ -1,18 +1,7 @@
-import tools.Calc;
 
-class Human extends Calc{
-    // protected int marks;
-    // protected String name;
-
-    public void show() {
-        // Calc obj = new Calc();
-        System.out.println(result);
-        // System.out.println(name + " : " + marks);
-    }
-}
 
 class Hello {
-    public static void main(String[] args) throws ClassNotFoundException {
-
+    public static void main(String[] args) {
+        
     }
 }
