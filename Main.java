2014f7faa91b210e9d0eafb1e2eb350b195e6ad8
Deepak <chintadadeepak31@gmail.com>
2023-09-09 20:33:29 +0530

    understood  abstract keyword, inner class and anonymous class, inner and anonymous class

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
-import projects.*;
 
-public class Main {
+
+class Main {
     public static void main(String[] args) {
-        QuestionService service = new QuestionService();
-        service.playQuiz();
-        int quizScore = service.calculateScore();
-        System.out.println("your score : " + quizScore);
+        
     }
 }
